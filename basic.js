31ce901ff9ce93396b149a94ef53e0cc4c3211e4
Musadiqaman <musadiqaman2006@gmail.com>
2024-12-08 21:47:55 -0800

    my pracise in js

---
+++ basic.js
@@ -0,0 +1,142 @@
+
+
+// alert("Welcome to the mern stack journey");
+
+// operators : used to perform operation on data like a+b a&b are the operand and + is the operator
+// Arithematic operators(+,-,/,*)
+
+
+let a = 1;
+let b = 1;
+
+console.log("a =",a ,"& b =",b);
+// console.log("a+b = ",a+b);
+// console.log("a-b = ",a-b);
+// console.log("a/b = ",a/b);
+// console.log("a*b = ",a*b);
+
+// below are the bitwise operations
+
+// console.log("a&b = ",a&b);
+// console.log("a|b = ",a|b);
+// console.log("a^b = ",a^b);
+
+// modulas : used for reminder %
+// Exponentiation : used for power  **
+
+
+// console.log("a%b = ",a%b);
+// console.log("a**b = ",a**b);
+
+
+// Unary operators:
+// increment  ++
+// decrement  --
+ 
+
+// post increment first print then add  a++
+
+// console.log("a++ is :",a++);
+// console.log("The value of a is :",a);
+
+// pre increment first add and print both ++a
+
+// console.log("++a is :",++a);
+
+
+
+// post decrement first print then subtract  a--
+
+// console.log("a-- is :",a--);
+// console.log("The value of a is :",a);
+
+// pre decrement first subtract and print both  --a
+
+// console.log("--a is :",--a);
+
+
+// Assignment operator is used to assign the value to the variables
+// =
+// +=
+// -=
+// /=
+// *=
+// **=
+
+// console.log("+= :",a+=b);
+// console.log("-= :",a-=b);
+// console.log("/= :",a/=b);
+// console.log("*= :",a*=b); 
+// console.log("**= :",a**=b);
+// console.log("%= :",a%=b);
+
+
+
+// Comparison operator  == != === !== > < >= <= 
+
+// console.log("3==3",a==b);
+// console.log("3!=3",a!=b);
+// console.log("7<=5 :",7<=5);
+
+
+
+
+// let age=parseInt(prompt("enter the value for age")) ;
+
+// if(age>=18&&age<=50)
+// {
+//     let gender=prompt("Enter the gender");
+//     if(gender=='male'){
+//         console.log("Eligible");
+//     }
+//     else if(gender=='female'){
+//             console.log("Not eligible for licence go home");
+//         }
+//      else{
+//         console.log("Not an valid gender");
+//      }   
+    
+// }
+
+// else if(age<18)
+// {
+//     console.log("not eligible at this age");
+// }
+
+// else{
+//     console.log("Not a valid age");
+// }
+
+
+
+
+// let gender = prompt("Enter the gender ");
+// if(gender=='female'){
+//     console.log("get out");
+// }
+
+// else if(gender=='male'){
+// let age =parseInt(prompt("Enter the value of age"));
+// if(age>=18&&age<=50)
+// {
+//     console.log("valid for passport");
+// }
+
+// else if(age>=0 &&age<18)
+// {
+//     console.log("Not valid for passport");
+// }
+
+// else{
+//     console.log("Sorry!😢 Age not supported");
+// }
+
+
+// }
+
+// else{
+//     console.log("Not a valid gender ☠️");
+// }
+
+
+
